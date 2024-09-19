b7d874aff51d56df28632863977072ebaa54f80b
ben-strehl <benjaminstrehl0@gmail.com>
2024-09-18 22:29:55 -0400

    Relative line numbers, removed signcolumn

--- lua/ben/set.lua
+++ lua/ben/set.lua
@@ -1,5 +1,6 @@
-vim.opt.nu = true
+-- vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.numberwidth = 1
 
 vim.opt.clipboard = unnamedplus
 
@@ -25,7 +26,7 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "no"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
