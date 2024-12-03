332b9ed907a694a6e5861b49f538329ce010ea18
ben-strehl <benjaminstrehl0@gmail.com>
2024-12-03 17:21:12 -0500

    Updated guifont for 1440p monitor

--- lua/ben/set.lua
+++ lua/ben/set.lua
@@ -1,5 +1,5 @@
 if vim.fn.has("gui_running") then
-    vim.cmd('set guifont=Consolas:h10')
+    vim.cmd('set guifont=Consolas:h9')
 end
 
 vim.opt.relativenumber = true
@@ -10,7 +10,7 @@ vim.opt.clipboard = unnamedplus
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-    
+
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
